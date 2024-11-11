39c7692d47e5f8cf79cf9949e45095e6de9a90b4
elgorra2 dsñfjasñdfj <diego.crepal@educa.jcyl.es>
2024-11-11 11:03:10 +0000

    asdfsda

---
+++ contrasena/java.js
@@ -0,0 +1,9 @@
+let  contrasena;
+let  palabra = prompt("Indique la contrasena");
+contrasena = "buenas";
+
+if(contrasena === palabra.toLowerCase()){
+    alert("contrasena correcta");
+} else {
+    alert("vuelvelo a intentar");
+}
